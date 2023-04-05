5fb2a2c02f5f5f0840273acb0a96805364c8cd2f
Niranjan <niranjanpun59@gmail.com>
2023-04-05 15:40:04 +0545

    Completed First header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "../../component/header/Header";
 import Navbar from "../../component/navbar/Navbar";
 import "./home.css";
 
 const Home = () => {
-  return <div><Navbar/></div>;
+  return <div>
+    <Navbar/>
+    <Header/>
+    </div>;
 };
 
 export default Home;

bd2bd0c807996acc3a39b4902eb37c1cbcfbf63c
Niranjan <niranjanpun59@gmail.com>
2023-04-05 14:39:02 +0545

    First commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "../../component/navbar/Navbar";
+import "./home.css";
+
+const Home = () => {
+  return <div><Navbar/></div>;
+};
+
+export default Home;
